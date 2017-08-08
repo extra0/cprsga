f3d0349c6eef1bf9916c2c328c8526f59b81d715
Ростислав <extra0@mail.ru>
2017-08-08 19:27:24 +0300

    адаптив второстепенной

--- develop/js/script.js
+++ develop/js/script.js
@@ -3,4 +3,16 @@ $(function(){
 	// вызов фенсибокса
 	$('.js-fancybox').fancybox();
 
+	// триггер главного меню
+	$('.header__menu-btn').on('click', function(){
+		$('.header__menu-list').slideToggle(300);
+		if ($(this).hasClass('active')) {
+			$(this).find('.fa').toggleClass('fa-bars fa-close')
+			$(this).removeClass('active');
+		} else {
+			$(this).find('.fa').toggleClass('fa-bars fa-close')
+			$(this).addClass('active');
+		}
+	});
+
 });
