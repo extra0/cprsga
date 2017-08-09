c827e3fb512ce284fd6a82cba50b12fc199ea412
Ростислав <extra0@mail.ru>
2017-08-09 21:41:31 +0300

    верстка галвной

--- develop/js/script.js
+++ develop/js/script.js
@@ -15,4 +15,45 @@ $(function(){
 		}
 	});
 
+	// первый слайдер на главной
+	$('.slider-section__block').bxSlider({
+		mode: 'fade',
+		pager: false,
+		controls: true
+	});
+
+
+	// слайдер благодарностей
+	$('.reviews-section__block').bxSlider({
+		pager: true,
+		controls: false,
+		minSlides: 2,
+		maxSlides: 2,
+		slideWidth: 245,
+		slideMargin: 40
+	});
+
+
+	// слайдер курсов
+	$('.course-form__bottom-block-slider').bxSlider({
+		controls: false,
+		minSlides: 2,
+		maxSlides: 2,
+		slideWidth: 700,
+		slideMargin: 40
+	});
+
+
+	
+	// останавливаем скролл header
+	$(window).on('scroll load', function(){
+		var footerScroll = $('.footer').offset().top,
+			bodyScroll = $(window).scrollTop();
+		if (bodyScroll + $(window).height() >= footerScroll) {
+			$('.index-header').attr('style', 'top:'+ (-(bodyScroll + $(window).height() - footerScroll) -1) +'px;');
+		} else {
+			$('.index-header').attr('style', 'top:0px;');
+		}
+	});
+
 });
